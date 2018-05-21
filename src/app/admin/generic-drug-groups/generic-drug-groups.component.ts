4660f4b14888144fc9be50c46d5cae4b872084ea
naphattharawat <naphattharawat@gmail.com>
2018-05-21 19:58:54 +0700

    กลุ่มบัญชียาหลัก

--- src/app/admin/generic-drug-groups/generic-drug-groups.component.ts
+++ src/app/admin/generic-drug-groups/generic-drug-groups.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
 import { GenericDrugGroupsService } from '../generic-drug-groups.service';
 import { AlertService } from '../alert.service';
-
 import { LoadingComponent } from 'app/loading/loading.component';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-generic-drug-groups',
   templateUrl: './generic-drug-groups.component.html',
@@ -11,12 +11,25 @@ import { LoadingComponent } from 'app/loading/loading.component';
 export class GenericDrugGroupsComponent implements OnInit {
 
   @ViewChild('loadingModal') loadingModal: LoadingComponent;
-  groups: any = [];
-  groupId: string;
-  groupName: string;
-  groupCode: string;
+  listGroup1: any = [];
+  listGroup2: any = [];
 
-  opened = false;
+  groupName1 = '';
+  groupName2 = '';
+
+  groupCode1 = '';
+  groupCode2 = '';
+
+  openModalGroup1 = false;
+  openModalGroup2 = false;
+
+  modal1GroupCode1 = '';
+  modal1GroupName1 = '';
+  modal2GroupCode1 = '';
+  modal2GroupName1 = '';
+  modal2GroupCode2 = '';
+  modal2GroupName2 = '';
+
   isUpdate = false;
   loading = false;
 
@@ -27,24 +40,24 @@ export class GenericDrugGroupsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.getList();
+    this.getGroup1();
+    this.getGroup2();
   }
-
-  addNew() {
-    this.groupId = null;
-    this.groupCode = null;
-    this.groupName = null;
+  // ############## GROUP 1 ######################
+  addNewGroup1() {
+    this.modal1GroupCode1 = '';
+    this.modal1GroupName1 = '';
     this.isUpdate = false;
-    this.opened = true;
+    this.openModalGroup1 = true;
   }
 
-  getList() {
+  getGroup1() {
     this.loading = true;
-    this.drugGroupService.list()
+    this.drugGroupService.getGenericGroup1('A')
       .then((results: any) => {
         if (results.ok) {
-          this.groups = results.rows;
-          this.ref.detectChanges();
+          this.listGroup1 = results.rows;
+          // this.ref.detectChanges();
           this.loading = false;
         } else {
           this.alertService.error(JSON.stringify(results.error));
@@ -55,12 +68,10 @@ export class GenericDrugGroupsComponent implements OnInit {
       });
   }
 
-  setisActive(active: any, id: any) {
+  setisActiveGroup1(active: any, id: any) {
     const status = active.target.checked ? 'Y' : 'N';
     this.loadingModal.show();
-    console.log(id, status);
-    
-    this.drugGroupService.isActive(id, status)
+    this.drugGroupService.isActiveGroup1(id, status)
       .then((result: any) => {
         if (result.ok) {
           this.alertService.success();
@@ -75,22 +86,21 @@ export class GenericDrugGroupsComponent implements OnInit {
       });
   }
 
-  edit(p: any) {
-    this.groupId = p.group_id;
-    this.groupName = p.group_name;
-    this.groupCode = p.group_code;
+  editGroup1(p: any) {
+    this.modal1GroupName1 = p.group_name_1;
+    this.modal1GroupCode1 = p.group_code_1;
     this.isUpdate = true;
-    this.opened = true;
+    this.openModalGroup1 = true;
   }
 
-  remove(p: any) {
-    this.alertService.confirm('ต้องการลบ ใช่หรือไม่? [' + p.group_name + ']')
+  removeGroup1(p: any) {
+    this.alertService.confirm('ต้องการลบ ใช่หรือไม่? [' + p.group_name_1 + ']')
       .then(() => {
-        this.drugGroupService.remove(p.group_id)
+        this.drugGroupService.removeGroup1(p.group_code_1)
           .then((results: any) => {
             if (results.ok) {
               this.alertService.success();
-              this.getList();
+              this.getGroup1();
             } else {
               this.alertService.error(JSON.stringify(results.error));
             }
@@ -101,27 +111,122 @@ export class GenericDrugGroupsComponent implements OnInit {
       });
   }
 
-  async save() {
+  async saveGroup1() {
     try {
-      if (this.groupName) {
+      if (this.modal1GroupName1) {
         let results;
         if (this.isUpdate) {
-          results = await this.drugGroupService.update(this.groupId, this.groupName, this.groupCode);
-          if (this.groupCode == null || this.groupCode === '') {
-            await this.drugGroupService.update(this.groupId, this.groupName, this.groupId);
-          }
+          results = await this.drugGroupService.updateGroup1(this.modal1GroupName1, this.modal1GroupCode1);
         } else {
-          results = await this.drugGroupService.save(this.groupName, this.groupCode);
-          if ((this.groupCode == null || this.groupCode === '') && results.ok) {
-            await this.drugGroupService.update(results.rows[0], this.groupName, results.rows[0]);
-          }
+          results = await this.drugGroupService.saveGroup1(this.modal1GroupName1, this.modal1GroupCode1);
         }
-
-        // promise.then((results: any) => {
         if (results.ok) {
           this.alertService.success();
-          this.opened = false;
-          this.getList();
+          this.openModalGroup1 = false;
+          this.getGroup1();
+        } else {
+          this.alertService.error('ข้อมูลซ้ำ');
+          console.log(results.error);
+        }
+      }
+    } catch (error) {
+      this.alertService.error(error);
+    }
+  }
+  // ################################################
+  // ############## GROUP 2 ######################
+  addNewGroup2() {
+    this.modal2GroupCode1 = '';
+    this.modal2GroupCode2 = '';
+    this.modal2GroupName1 = '';
+    this.modal2GroupName2 = '';
+    this.isUpdate = false;
+    this.openModalGroup2 = true;
+  }
+
+  selectGroupName1Modal2(e, modal2GroupName1) {
+    // this.modal2GroupCode1 = e.target.value;
+    const idx = _.findIndex(this.listGroup1, { 'group_code_1': this.modal2GroupCode1 });
+    this.modal2GroupName1 = this.listGroup1[idx];
+    console.log(this.modal2GroupCode1);
+    
+  }
+  getGroup2() {
+    this.loading = true;
+    this.drugGroupService.getGenericGroup2('A')
+      .then((results: any) => {
+        if (results.ok) {
+          this.listGroup2 = results.rows;
+          // this.ref.detectChanges();
+          this.loading = false;
+        } else {
+          this.alertService.error(JSON.stringify(results.error));
+        }
+      })
+      .catch(() => {
+        this.alertService.serverError();
+      });
+  }
+
+  setisActiveGroup2(active: any, data: any) {
+    const status = active.target.checked ? 'Y' : 'N';
+    this.loadingModal.show();
+    this.drugGroupService.isActiveGroup2(data.group_code_1, data.group_code_2, status)
+      .then((result: any) => {
+        if (result.ok) {
+          this.alertService.success();
+        } else {
+          this.alertService.error('เกิดข้อผิดพลาด : ' + JSON.stringify(result.error));
+        }
+        this.loadingModal.hide();
+      })
+      .catch(() => {
+        this.loadingModal.hide();
+        this.alertService.serverError();
+      });
+  }
+
+  editGroup2(p: any) {
+    this.modal2GroupName1 = p.group_name_1;
+    this.modal2GroupName2 = p.group_name_2;
+    this.modal2GroupCode1 = p.group_code_1;
+    this.modal2GroupCode2 = p.group_code_2;
+    this.isUpdate = true;
+    this.openModalGroup2 = true;
+  }
+
+  removeGroup2(p: any) {
+    this.alertService.confirm('ต้องการลบ ใช่หรือไม่? [' + p.group_name_2 + ']')
+      .then(() => {
+        this.drugGroupService.removeGroup2(p.group_code_2)
+          .then((results: any) => {
+            if (results.ok) {
+              this.alertService.success();
+              this.getGroup2();
+            } else {
+              this.alertService.error(JSON.stringify(results.error));
+            }
+          })
+          .catch(() => {
+            this.alertService.serverError();
+          });
+      });
+  }
+
+  async saveGroup2() {
+    console.log(this.modal2GroupCode1);
+    try {
+      if (this.modal2GroupName2 && this.modal2GroupCode1) {
+        let results;
+        if (this.isUpdate) {
+          results = await this.drugGroupService.updateGroup2(this.modal2GroupCode1, this.modal2GroupCode2, this.modal2GroupName2);
+        } else {
+          results = await this.drugGroupService.saveGroup2(this.modal2GroupCode1, this.modal2GroupCode2, this.modal2GroupName2);
+        }
+        if (results.ok) {
+          this.alertService.success();
+          this.openModalGroup2 = false;
+          this.getGroup2();
         } else {
           this.alertService.error('ข้อมูลซ้ำ');
           console.log(results.error);
