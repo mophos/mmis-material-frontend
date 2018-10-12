e1eb2c0f9207b33d48c4970bbaee6fd2db918192
naphattharawat <naphattharawat@gmail.com>
2018-10-12 13:12:32 +0700

    search dc24

---
+++ src/app/admin/generic-new/generic-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wm-generic-new',
+  templateUrl: './generic-new.component.html',
+  styles: []
+})
+export class GenericNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
