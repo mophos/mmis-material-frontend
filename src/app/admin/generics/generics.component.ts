b70446799aca3dda5a500f7bbfbcf67c18514a0e
naphattharawat <naphattharawat@gmail.com>
2018-10-19 13:41:07 +0700

    ชื่อ generic : แสดงผลไม่ตรงตามชื่อคอลัมภ์

--- src/app/admin/generics/generics.component.ts
+++ src/app/admin/generics/generics.component.ts
@@ -152,12 +152,9 @@ export class GenericsComponent implements OnInit {
     this.loadingModal.show();
     try {
       const rs: any = await this.standardService.getGenericTypes();
-      console.log(rs.rows);
-
       this.loadingModal.hide();
       if (rs.ok) {
         this.loadingModal.hide();
-        // this.genericTypes = rs.rows;
         if (rs.rows.length) {
           rs.rows.forEach(v => {
             this.genericTypeIds.forEach(x => {
